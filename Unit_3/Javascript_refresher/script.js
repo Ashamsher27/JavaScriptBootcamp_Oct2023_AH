eef096e445aa01d8bcf2da1aa8860dea9614255c
Ayesha <itsumoayesha@gmail.com>
2023-10-17 22:38:54 -0400

    adding unit 3 folder

---
+++ Unit_3/Javascript_refresher/script.js
@@ -0,0 +1,29 @@
+const firstName= "Ayesha"; 
+const lastName= "Shamsher";
+const fullName= firstName.concat(" ", lastName); {
+    console.log(fullName);
+}
+
+const population= "139600000"; {
+    console.log(population);
+    console.log(population * 3);
+}
+
+const javaScriptisCool= Boolean(true);
+
+const colors= ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const loopOne = 5;
+for (let i = 1; i <= loopOne; i++) {
+  console.log(i);
+}
+
+const loopTwo = 4;
+for (let i = 0; i <= loopTwo; i++) {
+  console.log(i);
+}
+
+
+
+
+
