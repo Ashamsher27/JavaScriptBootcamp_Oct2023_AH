e073f8da414ad9ed54f67de41728250d3e2676dd
Ayesha <itsumoayesha@gmail.com>
2023-10-21 17:30:04 -0400

    second commit for heating/cooling and first commit for shape functions

--- Unit_3/Heatling:cooling/script.js
+++ Unit_3/Heatling:cooling/script.js
@@ -6,31 +6,26 @@ if (actualTemp < desiredTemp) {
     console.log("Run heat");
 } else if (actualTemp > desiredTemp) {
     console.log("Run A/C");
-} else (console.log(actualTemp == desiredTemp, "Standby")); {
+} else if (actualTemp === desiredTemp) {
+    console.log("Standby"); 
 }
 
 
 const tempCelsius = prompt("What is the current temperature?")
 const celcius = parseInt(tempCelsius);
 const targetUnit = prompt ("which unit of temperature would you like to convert to? (K, C, F)")
-const kelvin = 273.15;
-const fahrenheit = 32;
-
 
 switch (targetUnit) {
     case "K": 
-    console.log(celcius + kelvin + " °K")
+    console.log(celcius + 273.15 + " °K")
     break; 
     case "C":
     console.log(celcius + " °C")
     break;
     case "F":
-    console.log(((celcius * 9/5) + fahrenheit) + " °F")
+    console.log(((celcius * 9/5) + 32) + " °F")
     break;
 }
 
-
-//0°C + 273.15 = 273.15K
-//(0°C × 9/5) + 32 = 32°F
 
 
---
+++ Unit_3/Shape_functions/script.js
@@ -0,0 +1,19 @@
+function getAreaofCircle(radius) {
+    return Math.PI * (radius * radius);
+  }
+  console.log(getAreaofCircle(4));
+
+function getCircumferenceOfCircle(radius) {
+    return 2 * Math.PI * radius;
+}
+    console.log(getCircumferenceOfCircle(4));
+
+function getAreaofSquare(side) {
+    return side * side;
+}
+console.log(getAreaofSquare(4));
+
+function getAreaofTriangle(height, base) {
+    return (height * base)/2; 
+}
+console.log(getAreaofTriangle(4, 10));
