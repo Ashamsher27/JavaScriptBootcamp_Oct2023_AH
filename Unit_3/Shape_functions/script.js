62a1c60547510b659b2be72e15f8d73f0f061b8f
Ayesha <itsumoayesha@gmail.com>
2023-10-23 18:31:45 -0400

    second commit for Shape fucntions lab because I forgot to add the param stuff and the arrow functions

--- Unit_3/Shape_functions/script.js
+++ Unit_3/Shape_functions/script.js
@@ -1,3 +1,12 @@
+/**this is a description of the getAreaofCircle function 
+ * represents the area of a circle 
+ * @constructor
+ * @param {number} radius - the radius of a circle. 
+ * @param {number} side - the side of a square.
+ * @param {number} height - the hieght of a triangle. 
+ * @param {number} base - the base lenght of a triangle. 
+*/
+
 function getAreaofCircle(radius) {
     return Math.PI * (radius * radius);
   }
@@ -17,3 +26,23 @@ function getAreaofTriangle(height, base) {
     return (height * base)/2; 
 }
 console.log(getAreaofTriangle(4, 10));
+
+
+//using arrow function 
+
+{let getAreaofCircle = (radius) => Math.PI * (radius * radius);
+    console.log(getAreaofCircle(4));
+}
+
+{let getCircumferenceOfCircle = (radius) => 2 * Math.PI * radius;
+    console.log(getCircumferenceOfCircle(4));
+}
+
+{let getAreaofSquare = (side) => side * side;
+    console.log(getAreaofSquare(4));
+}
+
+{let getAreaofTriangle = (height, base) => (height * base)/2; 
+    console.log(getAreaofTriangle(4, 10));
+}
+
