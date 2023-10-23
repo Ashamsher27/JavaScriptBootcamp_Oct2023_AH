49280a78d3782e1565e4018550967f662ea1af9b
Ayesha <itsumoayesha@gmail.com>
2023-10-23 19:24:55 -0400

    third commit for longest name lab because I used only two equal signs, so I went back and changed them all to three equal signs

--- Unit_3/Longest_name_lab/script.js
+++ Unit_3/Longest_name_lab/script.js
@@ -5,19 +5,19 @@ const nameThree="Jonathan Plumb";
 {   
     let biggestname = Math.max(nameOne.length, nameTwo.length, nameThree.length);
     console.log(biggestname);
-    if ((biggestname == nameOne.length) && (biggestname == nameTwo.length) && (biggestname == nameThree.length)) {
+    if ((biggestname === nameOne.length) && (biggestname === nameTwo.length) && (biggestname === nameThree.length)) {
             console.log(`All three names: ${nameOne}, ${nameTwo}, and ${nameThree}`);
-        } else if ((biggestname == nameOne.length) && (biggestname == nameTwo.length)) {
+        } else if ((biggestname === nameOne.length) && (biggestname == nameTwo.length)) {
             console.log(`${nameOne} and ${nameTwo} have the longest names.`);
-        } else if ((biggestname == nameOne.length) && (biggestname == nameThree.length)) {
+        } else if ((biggestname === nameOne.length) && (biggestname === nameThree.length)) {
             console.log(`${nameOne} and ${nameThree} have the longest names.`);
-        } else if ((biggestname == nameThree.length) && (biggestname == nameTwo.length)) {
+        } else if ((biggestname === nameThree.length) && (biggestname === nameTwo.length)) {
             console.log(`${nameThree} and ${nameTwo} have the longest names.`);
-        } else if (biggestname == nameOne.length) {
+        } else if (biggestname === nameOne.length) {
             console.log(`${nameOne} is the longest name.`); 
-        } else if (biggestname == nameTwo.length) {
+        } else if (biggestname === nameTwo.length) {
             console.log(`${nameTwo} is the longest name.`);
-        } else if (biggestname == nameThree.length) {
+        } else if (biggestname === nameThree.length) {
             console.log(`${nameThree} is the longest name.`);   
         }
 }
